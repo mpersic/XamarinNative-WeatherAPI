835e58bc7e2904b828b72c6227ee76cc41e535c4
Matej <matej@MacBook-Air-od-Matej.local>
2023-11-04 06:15:25 +0100

    added template assets

---
+++ NNChallenge.Droid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Content;
+using System.Collections.Generic;
+using NNChallenge.Constants;
+
+namespace NNChallenge.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_location);
+
+            Button buttonForecst = FindViewById<Button>(Resource.Id.button_forecast);
+            buttonForecst.Click += OnForecastClick;
+
+            Spinner spinnerLocations = FindViewById<Spinner>(Resource.Id.spinner_location);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                Android.Resource.Layout.SimpleSpinnerDropDownItem,
+                LocationConstants.LOCATIONS
+            );
+
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            spinnerLocations.Adapter = adapter;
+        }
+
+        private void OnForecastClick(object sender, EventArgs e)
+        {
+            this.StartActivity(new Intent(this, typeof(ForecastActivity)));
+        }
+    }
+}
